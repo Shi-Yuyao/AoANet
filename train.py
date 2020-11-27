0e7bd446f50bff86b02d962067d88a3d13adef47
yuyao <yukyewshek@outlook.com>
2020-11-27 12:44:55 +0100

    add comments

--- train.py
+++ train.py
@@ -28,38 +28,42 @@ except ImportError:
     print("tensorboardX is not installed")
     tb = None
 
+
 def add_summary_value(writer, key, value, iteration):
     if writer:
         writer.add_scalar(key, value, iteration)
 
+
 def train(opt):
     # Deal with feature things before anything
-    opt.use_fc, opt.use_att = utils.if_use_feat(opt.caption_model)
-    if opt.use_box: opt.att_feat_size = opt.att_feat_size + 5
+    opt.use_fc, opt.use_att = utils.if_use_feat(opt.caption_model)  # 按照参数中指定的caption model判定use_fc和use_att的状态
+    if opt.use_box:  # 如果use_box，需要给feat_size增加5，以传入坐标与置信度
+        opt.att_feat_size = opt.att_feat_size + 5
 
     acc_steps = getattr(opt, 'acc_steps', 1)
-        
-    loader = DataLoader(opt)
+
+    loader = DataLoader(opt)  # 按照opt参数设计加载数据，建立对象loader
     opt.vocab_size = loader.vocab_size
     opt.seq_length = loader.seq_length
 
-    tb_summary_writer = tb and tb.SummaryWriter(opt.checkpoint_path)
+    tb_summary_writer = tb and tb.SummaryWriter(opt.checkpoint_path)  # 启用tensorboard记录各metrics
 
-    infos = {}
+    infos = {}  # 建立一个infos字典来存放完整的训练信息
     histories = {}
-    if opt.start_from is not None:
+    if opt.start_from is not None:  # 在原有的训练结果上继续训练需要加载的相关文件
         # open old infos and check if models are compatible
-        with open(os.path.join(opt.start_from, 'infos_'+opt.id+'.pkl'), 'rb') as f:
+        with open(os.path.join(opt.start_from, 'infos_' + opt.id + '.pkl'), 'rb') as f:
             infos = utils.pickle_load(f)
             saved_model_opt = infos['opt']
-            need_be_same=["caption_model", "rnn_type", "rnn_size", "num_layers"]
+            need_be_same = ["caption_model", "rnn_type", "rnn_size", "num_layers"]
             for checkme in need_be_same:
-                assert vars(saved_model_opt)[checkme] == vars(opt)[checkme], "Command line argument and saved model disagree on '%s' " % checkme
+                assert vars(saved_model_opt)[checkme] == vars(opt)[
+                    checkme], "Command line argument and saved model disagree on '%s' " % checkme
 
-        if os.path.isfile(os.path.join(opt.start_from, 'histories_'+opt.id+'.pkl')):
-            with open(os.path.join(opt.start_from, 'histories_'+opt.id+'.pkl'), 'rb') as f:
+        if os.path.isfile(os.path.join(opt.start_from, 'histories_' + opt.id + '.pkl')):
+            with open(os.path.join(opt.start_from, 'histories_' + opt.id + '.pkl'), 'rb') as f:
                 histories = utils.pickle_load(f)
-    else:
+    else:  # 新训练需要存储的训练信息
         infos['iter'] = 0
         infos['epoch'] = 0
         infos['iterators'] = loader.iterators
@@ -69,7 +73,7 @@ def train(opt):
 
     iteration = infos.get('iter', 0)
     epoch = infos.get('epoch', 0)
-
+    # 在histories中建立若干子字典，记录相应的训练信息
     val_result_history = histories.get('val_result_history', {})
     loss_history = histories.get('loss_history', {})
     lr_history = histories.get('lr_history', {})
@@ -81,9 +85,9 @@ def train(opt):
         best_val_score = infos.get('best_val_score', None)
 
     opt.vocab = loader.get_vocab()
-    model = models.setup(opt).cuda()
+    model = models.setup(opt).cuda()  # 在cuda上生成模型
     del opt.vocab
-    dp_model = torch.nn.DataParallel(model)
+    dp_model = torch.nn.DataParallel(model)  # 将模型放在在多个GPU上并行（但好像没有传输device的参数）
     lw_model = LossWrapper(model, opt)
     dp_lw_model = torch.nn.DataParallel(lw_model)
 
@@ -92,7 +96,7 @@ def train(opt):
     dp_lw_model.train()
 
     if opt.noamopt:
-        assert opt.caption_model in ['transformer','aoa'], 'noamopt can only work with transformer'
+        assert opt.caption_model in ['transformer', 'aoa'], 'noamopt can only work with transformer'
         optimizer = utils.get_std_opt(model, factor=opt.noamopt_factor, warmup=opt.noamopt_warmup)
         optimizer._step = iteration
     elif opt.reduce_on_plateau:
@@ -101,9 +105,8 @@ def train(opt):
     else:
         optimizer = utils.build_optimizer(model.parameters(), opt)
     # Load the optimizer
-    if vars(opt).get('start_from', None) is not None and os.path.isfile(os.path.join(opt.start_from,"optimizer.pth")):
+    if vars(opt).get('start_from', None) is not None and os.path.isfile(os.path.join(opt.start_from, "optimizer.pth")):
         optimizer.load_state_dict(torch.load(os.path.join(opt.start_from, 'optimizer.pth')))
-
 
     def save_checkpoint(model, infos, optimizer, histories=None, append=''):
         if len(append) > 0:
@@ -111,15 +114,15 @@ def train(opt):
         # if checkpoint_path doesn't exist
         if not os.path.isdir(opt.checkpoint_path):
             os.makedirs(opt.checkpoint_path)
-        checkpoint_path = os.path.join(opt.checkpoint_path, 'model%s.pth' %(append))
+        checkpoint_path = os.path.join(opt.checkpoint_path, 'model%s.pth' % (append))
         torch.save(model.state_dict(), checkpoint_path)
         print("model saved to {}".format(checkpoint_path))
-        optimizer_path = os.path.join(opt.checkpoint_path, 'optimizer%s.pth' %(append))
+        optimizer_path = os.path.join(opt.checkpoint_path, 'optimizer%s.pth' % (append))
         torch.save(optimizer.state_dict(), optimizer_path)
-        with open(os.path.join(opt.checkpoint_path, 'infos_'+opt.id+'%s.pkl' %(append)), 'wb') as f:
+        with open(os.path.join(opt.checkpoint_path, 'infos_' + opt.id + '%s.pkl' % (append)), 'wb') as f:
             utils.pickle_dump(infos, f)
         if histories:
-            with open(os.path.join(opt.checkpoint_path, 'histories_'+opt.id+'%s.pkl' %(append)), 'wb') as f:
+            with open(os.path.join(opt.checkpoint_path, 'histories_' + opt.id + '%s.pkl' % (append)), 'wb') as f:
                 utils.pickle_dump(histories, f)
 
     try:
@@ -129,15 +132,15 @@ def train(opt):
                     # Assign the learning rate
                     if epoch > opt.learning_rate_decay_start and opt.learning_rate_decay_start >= 0:
                         frac = (epoch - opt.learning_rate_decay_start) // opt.learning_rate_decay_every
-                        decay_factor = opt.learning_rate_decay_rate  ** frac
+                        decay_factor = opt.learning_rate_decay_rate ** frac
                         opt.current_lr = opt.learning_rate * decay_factor
                     else:
                         opt.current_lr = opt.learning_rate
-                    utils.set_lr(optimizer, opt.current_lr) # set the decayed rate
+                    utils.set_lr(optimizer, opt.current_lr)  # set the decayed rate
                 # Assign the scheduled sampling prob
                 if epoch > opt.scheduled_sampling_start and opt.scheduled_sampling_start >= 0:
                     frac = (epoch - opt.scheduled_sampling_start) // opt.scheduled_sampling_increase_every
-                    opt.ss_prob = min(opt.scheduled_sampling_increase_prob  * frac, opt.scheduled_sampling_max_prob)
+                    opt.ss_prob = min(opt.scheduled_sampling_increase_prob * frac, opt.scheduled_sampling_max_prob)
                     model.ss_prob = opt.ss_prob
 
                 # If start self critical training
@@ -148,10 +151,11 @@ def train(opt):
                     sc_flag = False
 
                 epoch_done = False
-            
+
+            # training section
             start = time.time()
             if (opt.use_warmup == 1) and (iteration < opt.noamopt_warmup):
-                opt.current_lr = opt.learning_rate * (iteration+1) / opt.noamopt_warmup
+                opt.current_lr = opt.learning_rate * (iteration + 1) / opt.noamopt_warmup
                 utils.set_lr(optimizer, opt.current_lr)
             # Load data from train split (0)
             data = loader.get_batch('train')
@@ -159,20 +163,21 @@ def train(opt):
 
             if (iteration % acc_steps == 0):
                 optimizer.zero_grad()
-            
+
             torch.cuda.synchronize()
             start = time.time()
             tmp = [data['fc_feats'], data['att_feats'], data['labels'], data['masks'], data['att_masks']]
             tmp = [_ if _ is None else _.cuda() for _ in tmp]
             fc_feats, att_feats, labels, masks, att_masks = tmp
 
-            model_out = dp_lw_model(fc_feats, att_feats, labels, masks, att_masks, data['gts'], torch.arange(0, len(data['gts'])), sc_flag)
+            model_out = dp_lw_model(fc_feats, att_feats, labels, masks, att_masks, data['gts'],
+                                    torch.arange(0, len(data['gts'])), sc_flag)
 
             loss = model_out['loss'].mean()
             loss_sp = loss / acc_steps
 
             loss_sp.backward()
-            if ((iteration+1) % acc_steps == 0):
+            if ((iteration + 1) % acc_steps == 0):
                 utils.clip_gradient(optimizer, opt.grad_clip)
                 optimizer.step()
             torch.cuda.synchronize()
@@ -180,10 +185,10 @@ def train(opt):
             end = time.time()
             if not sc_flag:
                 print("iter {} (epoch {}), train_loss = {:.3f}, time/batch = {:.3f}" \
-                    .format(iteration, epoch, train_loss, end - start))
+                      .format(iteration, epoch, train_loss, end - start))
             else:
                 print("iter {} (epoch {}), avg_reward = {:.3f}, time/batch = {:.3f}" \
-                    .format(iteration, epoch, model_out['reward'].mean(), end - start))
+                      .format(iteration, epoch, model_out['reward'].mean(), end - start))
 
             # Update the iteration and epoch
             iteration += 1
@@ -212,12 +217,12 @@ def train(opt):
             infos['epoch'] = epoch
             infos['iterators'] = loader.iterators
             infos['split_ix'] = loader.split_ix
-            
+
             # make evaluation on validation set, and save model
             if (iteration % opt.save_checkpoint_every == 0):
                 # eval model
                 eval_kwargs = {'split': 'val',
-                                'dataset': opt.input_json}
+                               'dataset': opt.input_json}
                 eval_kwargs.update(vars(opt))
                 val_loss, predictions, lang_stats = eval_utils.eval_split(
                     dp_model, lw_model.crit, loader, eval_kwargs)
@@ -230,7 +235,7 @@ def train(opt):
                 # Write validation result into summary
                 add_summary_value(tb_summary_writer, 'validation loss', val_loss, iteration)
                 if lang_stats is not None:
-                    for k,v in lang_stats.items():
+                    for k, v in lang_stats.items():
                         add_summary_value(tb_summary_writer, k, v, iteration)
                 val_result_history[iteration] = {'loss': val_loss, 'lang_stats': lang_stats, 'predictions': predictions}
 
